01dc6e1f475f27f6f9f6f849fe806448b6e98c5b
Lar000ki <lar000ki@gmail.com>
2024-01-18 23:09:34 +0300

    Initial Commit

---
+++ Instrumentus/InstrumentusApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstrumentusApp.swift
+//  Instrumentus
+//
+//  Created by Yan Fedorov on 18.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstrumentusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
